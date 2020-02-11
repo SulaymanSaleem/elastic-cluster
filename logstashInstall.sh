b21477c41294c1f94a8877e2546d3431eb2c572b
SulaymanSaleem <sulayman.com@gmail.com>
2020-02-11 12:48:44 +0000

    creating logstash install script

---
+++ logstashInstall.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+sudo apt-get install apt-transport-https
+
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+
+sudo apt-get update && sudo apt-get install logstash
+
+sudo systemctl enable logstash
