1390c3107927ba0f23c41d9f500473e2f840e111
Sulayman <sulayman.com@gmail.com>
2020-04-21 21:46:49 +0000

    working kibana

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 docker-compose -f create-certs.yml run --rm create_certs
-docker-compose up -d
-docker run --rm -v es_certs:/certs --network=es_elastic docker.elastic.co/elasticsearch/elasticsearch:7.5.1 curl --cacert /certs/ca/ca.crt -u elastic:PleaseChangeMe https://es01:9200
+docker-compose -f elastic-docker-tls.yml up -d
+
+#docker exec es01 /bin/bash -c "bin/elasticsearch-setup-passwords \
+#auto \
+#--url https://localhost:9200"
+
 
