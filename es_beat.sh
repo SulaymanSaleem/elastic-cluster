aad5002b0c22e5e082a24d073c4aed3247e8cef1
Sulayman Saleem <39617382+SulaymanSaleem@users.noreply.github.com>
2020-05-26 16:39:59 +0100

    adding centos

--- es_beat.sh
+++ es_beat.sh
@@ -3,6 +3,8 @@ cp /var/lib/docker/volumes/es_certs/ elastic-beats/ -r
 
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 PASSWORD=
+
+### for ubuntu ###
 sed -i 's/${ES_HOST}/'$IP'/g' elastic-beats/ubuntu/filebeat.yml
 sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/ubuntu/filebeat.yml
 
@@ -15,6 +17,19 @@ sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/ubuntu/modules.d/elasticse
 sed -i 's/${ES_HOST}/'$IP'/g' elastic-beats/ubuntu/modules.d/kibana.yml
 sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/ubuntu/modules.d/kibana.yml
 
+### for centos ####
+sed -i 's/${ES_HOST}/'$IP'/g' elastic-beats/centos/filebeat.yml
+sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/centos/filebeat.yml
 
+sed -i 's/${ES_HOST}/'$IP'/g' elastic-beats/centos/metricbeat.yml
+sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/centos/metricbeat.yml
+
+sed -i 's/${ES_HOST}/'$IP'/g' elastic-beats/centos/modules.d/elasticsearch.yml
+sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/centos/modules.d/elasticsearch.yml
+
+sed -i 's/${ES_HOST}/'$IP'/g' elastic-beats/centos/modules.d/kibana.yml
+sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' elastic-beats/centos/modules.d/kibana.yml
+
+### for Grafana ###
 sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' provisioning/datasources/filebeat.yml
 sed -i 's/${ES_PASSWORD}/'$PASSWORD'/g' provisioning/datasources/metricbeat.yml
